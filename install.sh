5b96e9fd127e82646797f7ac204a919e78873706
John Weachock <jweachock@gmail.com>
2013-02-27 11:30:40 -0500

    Also somehow missed the actual install.sh script...

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function inst {
+	echo "Installing $2 as $1..."
+	if [ -e $1 ]; then
+		#mv $1 $1.bak
+		mv $1 $1.bak
+	fi
+	#ln -s $2 $1
+	ln -s $2 $1
+}
+
+FROM=$(pwd)
+TO=~
+echo "Installing from $FROM to $TO"
+inst $TO/.vimrc $FROM/vimrc
+inst $TO/.bashrc $FROM/bashrc
+inst $TO/.colordiffrc $FROM/colordiffrc
+inst $TO/.dircolors $FROM/dircolors
+inst $TO/.gitconfig $FROM/gitconfig
+inst $TO/.tmux.conf $FROM/tmux.conf
+inst $TO/.trkrc $FROM/trkrc
+inst $TO/.vim $FROM/vim
+inst $TO/.vimrc $FROM/vimrc
+
+echo "Installing $FROM/fonts/* as $TO/.fonts/*..."
+if [ ! -e $TO/.fonts ]; then
+	mkdir $TO/.fonts
+fi
+cp -rf $FROM/fonts/* $TO/.fonts/
+
+echo "Installing $FROM/themes/* as $TO/.themes/*..."
+if [ ! -e $TO/.themes ]; then
+	mkdir $TO/.themes
+fi
+cp -rf $FROM/themes/* $TO/.themes/
