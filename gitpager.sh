5c4f928d6d1f74a956ec32e8d65a176874256080
John Weachock <jweachock@gmail.com>
2013-02-27 02:02:14 -0500

    git sends colors to the pager; gitpager.sh strips colors when it sends to vim

--- gitpager.sh
+++ gitpager.sh
@@ -7,5 +7,5 @@ LINES=$(tput lines)
 if [ "$lines" -lt "$LINES" ] ; then
 	echo "$stdin"
 else
-	echo "$stdin" | /bin/sh -c "col -b | vim -c 'set ft=mygit ts=8 nomod nolist nonu noma' -c 'noremap q <Esc>:q<Return>' -"
+	echo "$stdin" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | /bin/sh -c "col -b | vim -c 'set ft=mygit ts=8 nomod nolist nonu noma' -c 'noremap q <Esc>:q<Return>' -"
 fi
