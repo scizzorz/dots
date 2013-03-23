578c6288971d8547c6c0e41bbe439f223e3a4177
John Weachock <jweachock@gmail.com>
2013-03-23 00:47:08 -0400

    Remapped j and k to <C-e> and <C-y>

--- gitpager.sh
+++ gitpager.sh
@@ -7,5 +7,5 @@ LINES=$(tput lines)
 if [ "$lines" -lt "$LINES" ] ; then
 	echo "$stdin"
 else
-	echo "$stdin" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | /bin/bash -c "vim -c 'silent! set ft=mygit ts=8 cc=0 nomod nolist nu rnu noma' -c 'noremap q <Esc>:q!<Return>' -c 'unmap q:'</dev/tty <(col -b)"
+	echo "$stdin" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | /bin/bash -c "vim -c 'silent! set ft=mygit ts=8 cc=0 nomod nolist nu rnu noma' -c 'noremap q <Esc>:q!<Return>' -c 'unmap q:' -c 'noremap j <C-e>' -c 'noremap k <C-y>'</dev/tty <(col -b)"
 fi
