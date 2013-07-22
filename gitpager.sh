373cdff7625ed09f356427f79210522973c913c7
John Weachock <jweachock@gmail.com>
2013-07-22 12:11:20 -0500

    Add a vimp.rc to keep the config identical across all vimps

--- gitpager.sh
+++ gitpager.sh
@@ -7,5 +7,5 @@ LINES=$(tput lines)
 if [ "$lines" -lt "$LINES" ] ; then
 	echo "$stdin"
 else
-	echo "$stdin" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | /bin/bash -c "vim -c 'silent! set ft=mygit ls=0 ts=8 fdc=0 nomod nolist nonu noma nofen cc=0 nornu' -c 'noremap q <Esc>:q!<Return>' -c 'noremap j <C-e>' -c 'noremap k <C-y>'</dev/tty <(col -b)"
+	echo "$stdin" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | /bin/bash -c "vim -c 'set ft=mygit' -S ~/dots/vimp.rc </dev/tty <(col -b)"
 fi
