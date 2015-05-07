240c1558f66153dcaf9d6082ffed0673e10bc906
John Weachock <jweachock@gmail.com>
2015-05-06 23:28:15 -0400

    Remove some old crap

--- disks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-lsblk -lno MOUNTPOINT | grep "^\/" | sed -e 's/^.*$/\0${goto 60}$color2${fs_used_perc \0}%$color${goto 110}${fs_size \0}/g'
--- ping.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if ssh -o ConnectTimeout=5 $1 pwd &> /dev/null; then
-	echo 'up'
-else
-	echo '${color1}down${color}'
-fi
