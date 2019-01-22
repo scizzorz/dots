5629af61c1c698a55a1134b3eef194e0d5838e6e
John Weachock <jweachock@gmail.com>
2019-01-22 14:05:12 -0500

    zsh: Add prompt files for hex, trick, treat

---
+++ prompt-treat.sh
@@ -0,0 +1,3 @@
+# vim: set ft=zsh:
+export PROMPT_COLOR=$'\033[94m' # bright yellow (blue?)
+export PROMPT_PREFIX=''
---
+++ prompt-trick.sh
@@ -0,0 +1,3 @@
+# vim: set ft=zsh:
+export PROMPT_COLOR=$'\033[95m' # bright magenta
+export PROMPT_PREFIX=''
