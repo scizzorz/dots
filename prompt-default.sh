5ae0a223832e09de32597bf31a082cd1f98e1980
John Weachock <jweachock@gmail.com>
2019-05-21 19:39:48 -0400

    zsh: Fix prompt hostname hiding

--- prompt-default.sh
+++ prompt-default.sh
@@ -1,3 +1,8 @@
 # vim: set ft=zsh:
 export PROMPT_COLOR=$'\033[96m' # bright cyan
 export PROMPT_PREFIX='%{$fg_no_bold[white]%}%m ' # hostname
+
+# skip the host if we're in tmux
+if [ -n "$TMUX" ]; then
+  export PROMPT_PREFIX=''
+fi
