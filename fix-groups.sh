6ee3f0b0db33f18a47469971cb15d833bca9c2c1
John Weachock <jweachock@gmail.com>
2019-06-03 20:02:36 -0400

    d: Add fix-groups.sh

---
+++ fix-groups.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh
+# this is used to fix the permissions of the `john` user
+# to be in whatever group owns /var/run/docker.sock
+group=$(exa -lg /var/run/docker.sock | cut -d" " -f4)
+echo "Identified $group as group of /var/run/docker.sock"
+
+if [[ $yournumber =~ '^[0-9]+$' ]] ; then
+  echo "Unclaimed gid; creating new group..."
+  groupadd -g $group dockeroo
+  group=dockeroo
+fi
+
+usermod john -a -G $group
+groups john
