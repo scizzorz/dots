8148cd01d641ff9174514d7eb8ce6a388d4df804
John Weachock <jweachock@gmail.com>
2025-04-28 10:10:25 -0400

    Add gg and quickgoose
    Still pretty wip but :shrug:

--- zsh/zsh.zsh
+++ zsh/zsh.zsh
@@ -2,6 +2,7 @@ unsetopt beep                      # disable beeps
 bindkey -e                         # switch to emacs bindings
 autoload -U colors && colors       # enable colors
 
+bindkey -s '^g' '~/dots/bin/quickgoose\n'  # add ^s binding to launch a quick goose
 bindkey -s '^f' '~/dots/bin/vf\n'  # add ^f binding to launch skim and open in vim
 bindkey -s '^o' 'nvim\n'  # add ^o binding to launch vim
 
