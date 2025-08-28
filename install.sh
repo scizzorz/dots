52fe7746947390872827051f511ccf078425474c
John Weachock <jweachock@gmail.com>
2025-08-27 23:56:07 -0400

    misc: Remove dead files

--- install.sh
+++ install.sh
@@ -24,11 +24,9 @@ mkdir -p $TO/.config
 mkdir -p $TO/.local/share/nvim/site/autoload
 
 echo "Installing from $FROM to $TO"
-inst $TO/.xinitrc $FROM/xinitrc
 inst $TO/.zshrc $FROM/zshrc
 inst $TO/.gitconfig $FROM/gitconfig
 inst $TO/.tmux.conf $FROM/tmux.conf
-inst $TO/.gtk-bookmarks $FROM/gtk-bookmarks
 inst $TO/.xmodmap $FROM/xmodmap
 inst $TO/.config/nvim $FROM/nvim
 inst $TO/.local/share/nvim/site/autoload/plug.vim $FROM/nvim/plug.vim
@@ -37,15 +35,8 @@ echo "Installing $FROM/fonts/* as $TO/.fonts/*..."
 mkdir -p $TO/.fonts
 cp -rf $FROM/fonts/* $TO/.fonts/
 
-echo "Installing $FROM/themes/* as $TO/.themes/*..."
-mkdir -p $TO/.themes
-cp -rf $FROM/themes/* $TO/.themes/
-
 mkdir -p $TO/.config/alacritty
 bin/set-alacritty-theme.sh dark
 inst $TO/.config/alacritty/alacritty.yml $FROM/alacritty.yml
 
 bin/set-prompt-theme.sh default
-
-mkdir -p $TO/.config/tint2
-inst $TO/.config/tint2/tint2rc $FROM/tint2rc
