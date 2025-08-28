b69b91dcd873f37abb251fc46ef2baebcd8ad3e0
John Weachock <jweachock@gmail.com>
2025-08-27 23:57:15 -0400

    docker: Remove

--- docker/entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export HOME=/home/$ME
-chown -R $ME:$ME $HOME/dev
-
-sudo -E -u $ME "$@"
--- docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker run --rm -it \
-  -v /home/john/.ssh/id_rsa:/home/john/.ssh/id_rsa \
-  -v /home/john/.ssh/id_rsa.pub:/home/john/.ssh/id_rsa.pub \
-  "$@" scizzorz/arch
