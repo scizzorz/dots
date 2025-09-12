e6b0baa4bdf8f3906908dd1c5eee49aae6cd0252
John Weachock <jweachock@gmail.com>
2025-09-11 23:02:15 -0400

    zsh: Fix Hermit env in $PATH when using 'hm'

--- zsh.d/40-path.zsh
+++ zsh.d/40-path.zsh
@@ -1,9 +1,18 @@
-export PATH=./:$PATH
-export PATH=~/.cargo/bin:$PATH
-export PATH=~/.local/bin:$PATH
-export PATH=~/.poetry/bin:$PATH
-export PATH=~/bin:$PATH
-export PATH=~/dots/bin:$PATH
-export PATH=~/pico-8:$PATH
-export PATH=~/scripts:$PATH
-export PATH="/opt/homebrew/bin:$PATH"
+PATH=~/.cargo/bin:$PATH
+PATH=~/.local/bin:$PATH
+PATH=~/.poetry/bin:$PATH
+PATH=~/bin:$PATH
+PATH=~/dots/bin:$PATH
+PATH=~/pico-8:$PATH
+PATH=~/scripts:$PATH
+PATH="/opt/homebrew/bin:${PATH}"
+PATH=./:$PATH
+
+# Sometimes Hermit envs get lost in the middle of PATH and need to
+# be brought to the front again
+if [[ -n "${HERMIT_ENV}" && "${PATH}" =~ "${HERMIT_ENV}/bin" ]]; then
+  PATH="${HERMIT_ENV}/bin:${PATH}"
+fi
+
+# Remove duplicates
+typeset -U path manpath
